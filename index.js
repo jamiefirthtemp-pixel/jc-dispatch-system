a23751f2aae6bfe99d6eeb188d8cc5352b4f7c83
jamiefirthtemp-pixel <jamiefirthtemp@gmail.com>
2026-04-25 23:23:38 +0100

    minimal clean test

--- index.js
+++ index.js
@@ -1,59 +1,22 @@
-require('dotenv').config();
-
-const {
-Client,
-GatewayIntentBits,
-Events,
-REST,
-Routes,
-SlashCommandBuilder
-} = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 
 const client = new Client({
 intents: [GatewayIntentBits.Guilds]
 });
 
-client.once(Events.ClientReady, async (readyClient) => {
+client.once(Events.ClientReady, (readyClient) => {
 console.log('Bot online: ' + readyClient.user.tag);
-
-const commands = [
-new SlashCommandBuilder()
-.setName('job')
-.setDescription('Generate a test job')
-.toJSON()
-];
-
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-try {
-await rest.put(
-Routes.applicationCommands(readyClient.user.id),
-{ body: commands }
-);
-
-```
-console.log('Commands registered');
-```
-
-} catch (error) {
-console.error(error);
-}
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-try {
 if (!interaction.isChatInputCommand()) {
 return;
 }
 
-```
 if (interaction.commandName === 'job') {
-  await interaction.reply('🚚 JC Logistics Dispatch Working');
-}
-```
-
-} catch (error) {
-console.error(error);
+await interaction.reply({
+content: '🚚 JC Logistics Dispatch Working'
+});
 }
 });
 
