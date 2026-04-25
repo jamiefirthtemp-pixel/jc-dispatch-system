98b18604a6a24474aae3f7943ed78c5f2cb33ffb
jamiefirthtemp-pixel <jamiefirthtemp@gmail.com>
2026-04-25 23:07:19 +0100

    fixed interaction error

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 
 const client = new Client({
 intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', async () => {
-console.log('Bot online: ' + client.user.tag);
-
-const commands = [
-new SlashCommandBuilder()
-.setName('job')
-.setDescription('Generate a dispatch job')
-].map(command => command.toJSON());
-
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-try {
-await rest.put(
-Routes.applicationCommands(client.user.id),
-{ body: commands }
-);
-
-```
-console.log('Slash commands registered');
-```
-
-} catch (error) {
-console.log(error);
-}
+client.once(Events.ClientReady, readyClient => {
+console.log(`Bot online: ${readyClient.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 
 if (!interaction.isChatInputCommand()) {
 return;
@@ -40,8 +18,7 @@ if (interaction.commandName === 'job') {
 
 ```
 await interaction.reply({
-  content: '🚚 JC Logistics Dispatch Test Successful',
-  ephemeral: true
+  content: '🚚 JC Logistics Dispatch Test Successful'
 });
 ```
 
